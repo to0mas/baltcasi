3d7748f7d672aff9e193b3785efd6820fb095c6e
to0mas <rehanektomas123@gmail.com>
2025-05-30 09:46:56 +0200

    s

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -11,7 +11,7 @@ const Weather = () => {
 const search = async (city)=>{
 
     try{
-        const url = `https://api.openweathermap.org/data/2.5/weather?q={city}&appid=${import.meta.env.VITE_APP_ID}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
 
         const response = await fetch(url);
         const data = await response.json();
